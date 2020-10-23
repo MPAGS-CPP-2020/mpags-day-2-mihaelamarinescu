8dd433282687b51f649e4c5a5791af6895a556d5
Mihaela Marinescu <mxm1005@student.bham.ac.uk>
2020-10-23 09:01:27 +0000

    added transliteration code to function

--- src/mpags-cipher.cpp
+++ src/mpags-cipher.cpp
@@ -6,6 +6,47 @@
 // For std::isalpha and std::isupper
 #include <cctype>
 
+std::string transformChar( const char in_char )
+{
+      // Transliterate digits to English words
+
+    std::string inputText{""};
+    switch (in_char) {
+      case '0':
+	inputText += "ZERO";
+	break;
+      case '1':
+	inputText += "ONE";
+	break;
+      case '2':
+	inputText += "TWO";
+	break;
+      case '3':
+	inputText += "THREE";
+	break;
+      case '4':
+	inputText += "FOUR";
+	break;
+      case '5':
+	inputText += "FIVE";
+	break;
+      case '6':
+	inputText += "SIX";
+	break;
+      case '7':
+	inputText += "SEVEN";
+	break;
+      case '8':
+	inputText += "EIGHT";
+	break;
+      case '9':
+	inputText += "NINE";
+	break;
+    }
+
+    return inputText;
+}
+
 // Main function of the mpags-cipher program
 int main(int argc, char* argv[])
 {
@@ -115,40 +156,7 @@ int main(int argc, char* argv[])
       inputText += std::toupper(inputChar);
       continue;
     }
-
-    // Transliterate digits to English words
-    switch (inputChar) {
-      case '0':
-	inputText += "ZERO";
-	break;
-      case '1':
-	inputText += "ONE";
-	break;
-      case '2':
-	inputText += "TWO";
-	break;
-      case '3':
-	inputText += "THREE";
-	break;
-      case '4':
-	inputText += "FOUR";
-	break;
-      case '5':
-	inputText += "FIVE";
-	break;
-      case '6':
-	inputText += "SIX";
-	break;
-      case '7':
-	inputText += "SEVEN";
-	break;
-      case '8':
-	inputText += "EIGHT";
-	break;
-      case '9':
-	inputText += "NINE";
-	break;
-    }
+    inputText += transformChar(inputChar);
 
     // If the character isn't alphabetic or numeric, DONT add it.
     // Our ciphers can only operate on alphabetic characters.
